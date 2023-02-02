6a5d18de9418ca3f6b21a2ecc01cee4a7a4d4b12
Name <email@example.ru>
2023-02-02 23:28:21 +0300

    Add files

---
+++ NewsPortal/NewsApp/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+
+register = template.Library()
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+
+
+@register.filter()
+def censor(value):
+    banned_words = ['никогда', 'сериале', 'Зельвенский']
+
+    if not isinstance (value, str):
+        raise TypeError(f"Недопустимое значение '{type(value)}' Нужно ввести строковое значение")
+    words = value.replace(',', '').split()
+    for word in words:
+        if word in banned_words:
+            value = value.replace(word, f"{word[0]}{'*' * (len(word) - 1)}")
+    return value
