e566adc4889657b0c686e08236b74541bab20023
Name <email@example.ru>
2023-02-21 00:23:10 +0300

    Table changed to plain text

--- NewsPortal/NewsApp/views.py
+++ NewsPortal/NewsApp/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from datetime import datetime
+from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .filters import PostFilter
 from .forms import PostForm
 
@@ -18,32 +19,32 @@ class PostsList(ListView):
     # Это имя списка, в котором будут лежать все объекты.
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'posts'
-    paginate_by = 10
+    paginate_by = 5
 
-#    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
         # и вызываем у них метод get_context_data с теми же аргументами,
         # что и были переданы нам.
         # В ответе мы должны получить словарь.
-#        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         # К словарю добавим текущую дату в ключ 'time_now'.
- #       context['time_now'] = datetime.utcnow()
+        context['time_now'] = datetime.utcnow()
         # Добавим ещё одну пустую переменную,
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
-#        context['open_vacancies'] = None
-#        return context
+        context['open_vacancies'] = None
+        return context
 
-#    def get_queryset(self):
-        # Получаем обычный запрос
-#        queryset = super().get_queryset()
-        # Используем наш класс фильтрации.
-        # self.request.GET содержит объект QueryDict, который мы рассматривали
-        # в этом юните ранее.
-        # Сохраняем нашу фильтрацию в объекте класса,
-        # чтобы потом добавить в контекст и использовать в шаблоне.
-#        self.filterset = PostFilter(self.request.GET, queryset)
-        # Возвращаем из функции отфильтрованный список товаров
-#        return self.filterset.qs
+    # def get_queryset(self):
+    #     # Получаем обычный запрос
+    #    queryset = super().get_queryset()
+    #     # Используем наш класс фильтрации.
+    #     # self.request.GET содержит объект QueryDict, который мы рассматривали
+    #     # в этом юните ранее.
+    #     # Сохраняем нашу фильтрацию в объекте класса,
+    #     # чтобы потом добавить в контекст и использовать в шаблоне.
+    #     self.filterset = PostFilter(self.request.GET, queryset)
+    #     # Возвращаем из функции отфильтрованный список товаров
+    #     return self.filterset.qs
 
 #    def get_context_data(self, **kwargs):
 #        context = super().get_context_data(**kwargs)
@@ -105,5 +106,22 @@ class SearchResultsView(ListView):
         # Добавляем в контекст объект фильтрации.
         context['filterset'] = self.filterset
         return context
+
+
+class CategoryListView(ListView):
+    model = Post
+    template_name = 'category_list.html'
+    context_object_name = 'category_posts_list'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, id=self.kwargs['pk'])
+        queryset = Post.objects.filter(caregory=self.category).order_by('-date')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['is_not_subscriber'] = self.request.user not in self.category.subscribers
+       context['category'] = self.category
+       return context
 
 
