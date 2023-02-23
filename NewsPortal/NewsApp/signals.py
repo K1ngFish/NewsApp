acc6e11dd881aa2cefe23c4de5613b98ec3abc90
Name <email@example.ru>
2023-02-23 17:16:02 +0300

    Added subscription option

---
+++ NewsPortal/NewsApp/signals.py
@@ -0,0 +1,38 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from .models import PostCategory
+from NewsPortal.settings import SITE_URL, DEFAULT_FROM_EMAIL
+
+def send_notifications(preview, pk, title, subscribers):
+    html_context = render_to_string(
+        'post_created_email.html',
+        {
+            'text': preview,
+            'link': f'{SITE_URL}/posts/{pk}',
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject = title,
+        body = '',
+        from_email = DEFAULT_FROM_EMAIL,
+        to = subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html'),
+    msg.send()
+
+@receiver(post_save, sender=PostCategory)
+def notify_about_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.category.all()
+        subscribers: list[str] = []
+        for category in categories:
+            subscribers += category.subcribers.all()
+
+        subscribers = [s.email for s in subscribers]
+
+        send_notifications(instance.preview(), instance.pk, instance.title, subscribers)
+
